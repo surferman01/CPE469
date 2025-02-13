a1208837173e6ad63c982a20d8619c61308f1560
benksoto <ben@bensoto.org>
2025-02-13 10:44:43 -0800

    initial commit for lab4 + nodes speak to random node on network

---
+++ Labs/Lab4/RAFT/client/client.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"fmt"
+	// "math/rand"
+	"net/rpc"
+	"netRPCGossip/shared"
+	"os"
+	"strconv"
+
+	// "strings"
+	"sync"
+	"time"
+)
+
+const (
+	MAX_NODES  = 8
+	X_TIME     = 1
+	Y_TIME     = 2
+	Z_TIME_MAX = 120
+	Z_TIME_MIN = 30
+)
+
+var repeats = [MAX_NODES + 1]int{}
+
+var self_node shared.Node
+
+// Send the current membership table to a neighboring node with the provided ID
+func sendMessage(server *rpc.Client, id int, membership shared.Membership) {
+	req := shared.Request{ID: id, Table: membership}
+	var reply *bool
+
+	err := server.Call("Requests.Add", req, &reply)
+	if err != nil {
+		fmt.Println("Error in sendMessage:", err)
+		// } else {
+		// 	fmt.Println("add request:", reply)
+	}
+}
+
+// Read incoming messages from other nodes
+func readMessages(server *rpc.Client, id int, membership shared.Membership) *shared.Membership {
+	var reply *shared.Membership
+
+	err := server.Call("Requests.Listen", id, &reply)
+	if err != nil {
+		fmt.Println("Error in readMessages:", err)
+		// } else {
+		// 	fmt.Println("REPLY", *reply)
+	}
+
+	// combine memberships here
+	// local + reply
+
+	return shared.CombineTables(&membership, reply)
+}
+
+func calcTime() float64 {
+	return float64(time.Now().Unix())
+}
+
+var wg = &sync.WaitGroup{}
+
+func main() {
+	// rand.Seed(time.Now().UnixNano())
+	// Z_TIME := rand.Intn(Z_TIME_MAX-Z_TIME_MIN) + Z_TIME_MIN
+
+	// Connect to RPC server
+	server, _ := rpc.DialHTTP("tcp", "localhost:9005")
+
+	args := os.Args[1:]
+
+	// Get ID from command line argument
+	if len(args) == 0 {
+		fmt.Println("No args given")
+		return
+	}
+	id, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Found Error", err)
+	}
+
+	// fmt.Println("Node", id, "will fail after", Z_TIME, "seconds")
+
+	currTime := calcTime()
+	// Construct self
+	self_node = shared.Node{ID: id, Hbcounter: 0, Time: currTime, Alive: true}
+	var self_node_response shared.Node // Allocate space for a response to overwrite this
+
+	// Add node with input ID
+	if err := server.Call("Membership.Add", self_node, &self_node_response); err != nil {
+		fmt.Println("Error:2 Membership.Add()", err)
+	} else {
+		fmt.Printf("Success: Node created with id= %d\n", id)
+	}
+
+	// neighbors := self_node.InitializeNeighbors(id)
+	// fmt.Println("Neighbors:", neighbors)
+
+	membership := shared.NewMembership()
+	membership.Add(self_node, &self_node)
+
+	sendMessage(server, shared.RandInt(), *membership)
+
+	// crashTime := self_node.CrashTime()
+
+	time.AfterFunc(time.Second*X_TIME, func() { runAfterX(server, &self_node, &membership, id) })
+	time.AfterFunc(time.Second*Y_TIME, func() { runAfterY(server, shared.RandInt(), &membership, id) })
+	// time.AfterFunc(time.Second*time.Duration(Z_TIME), func() { runAfterZ(id) })
+
+	wg.Add(1)
+	wg.Wait()
+}
+
+func runAfterX(server *rpc.Client, node *shared.Node, membership **shared.Membership, id int) {
+	// fmt.Println("runAfterX NOW 1")
+
+	// Increment the heartbeat counter
+	node.Hbcounter++
+
+	// Update the node's time
+	node.Time = calcTime()
+	// (*membership).Update(*node, nil)
+	(*membership).Members[id] = *node
+
+	// Send the updated node information to the server membership table
+	if err := server.Call("Membership.Update", *node, nil); err != nil {
+		fmt.Println("Error: Membership.Update()", err)
+	} else {
+		fmt.Printf("Success: Node %d updated\n", id)
+	}
+
+	// Print the current membership table
+	printMembership(**membership)
+
+	new_membership := readMessages(server, id, **membership)
+	if new_membership != nil {
+		for _, n := range (*membership).Members {
+			if n.ID == id {
+				continue
+			}
+			tempNode := new_membership.Members[n.ID]
+
+			if (*membership).Members[n.ID].Hbcounter == new_membership.Members[n.ID].Hbcounter {
+				repeats[n.ID]++
+				// if the heartbeat is the same for 20 checks,
+				// then assume it has died
+				if repeats[n.ID] >= 20 {
+					tempNode.Alive = false
+					new_membership.Members[n.ID] = tempNode
+				}
+			} else {
+				repeats[n.ID] = 0
+				tempNode.Alive = true
+				new_membership.Members[n.ID] = tempNode
+			}
+		}
+		*membership = new_membership
+	}
+
+	// temp is now updated membership
+	// now update membership
+
+	// below is another method of checking for a node being dead
+	// if the timestamp has not updated on the node in x time
+	// then assume it is dead
+	// this could be used in conjunction with the checking of
+	// equivalent heartbeats for further status checking
+
+	// cur_time := calcTime()
+	// for id, n := range (**membership).Members {
+	// 	if cur_time-n.Time > 20 {
+	// 		t := (**membership).Members[id]
+	// 		t.Alive = false
+	// 		(**membership).Members[id] = t
+	// 	}
+	// }
+
+	// Schedule the next runAfterX call
+	time.AfterFunc(time.Second*X_TIME, func() { runAfterX(server, node, membership, id) })
+}
+
+func runAfterY(server *rpc.Client, neighbor int, membership **shared.Membership, id int) {
+	//TODO
+	fmt.Println("neightbor:", neighbor)
+	// sel := rand.Intn(2)
+	// fmt.Println("runAfterY NOW 2 with sel:", sel)
+	// send a heartbeat to a randomly selected neighbor of yours
+	sendMessage(server, neighbor, **membership)
+
+	time.AfterFunc(time.Second*Y_TIME, func() { runAfterY(server, shared.RandInt(), membership, id) })
+}
+
+// func runAfterZ(id int) {
+// 	// this will listening for others
+// 	// readMessages(*server, id, )
+// 	//TODO
+// 	fmt.Println("Node", id, "crashed!")
+// 	os.Exit(0)
+// }
+
+func printMembership(m shared.Membership) {
+	for _, val := range m.Members {
+		status := "is Alive"
+		if !val.Alive {
+			status = "is Dead"
+		}
+		fmt.Printf("Node %d has hb %d, time %.1f and %s\n", val.ID, val.Hbcounter, val.Time, status)
+	}
+	fmt.Println("")
+	fmt.Println("repeats", repeats)
+	fmt.Println("")
+
+}
---
+++ Labs/Lab4/RAFT/shared/shared.go
@@ -0,0 +1,152 @@
+package shared
+
+import (
+	"fmt"
+	"math/rand"
+	// "time"
+	// "net/http"
+	// "net/rpc"
+)
+
+const (
+	MAX_NODES = 8
+)
+
+// Node struct represents a computing node.
+type Node struct {
+	ID        int
+	Hbcounter int
+	Time      float64
+	Alive     bool
+	Leader    bool
+}
+
+// Generate random crash time from 10-60 seconds
+// func (n Node) CrashTime() int {
+// 	// rand.Seed(time.Now().UnixNano())
+// 	max := 60
+// 	min := 10
+// 	return rand.Intn(max-min) + min
+// }
+
+func (n Node) InitializeNeighbors(id int) [2]int {
+	neighbor1 := RandInt()
+	for neighbor1 == id {
+		neighbor1 = RandInt()
+	}
+	neighbor2 := RandInt()
+	for neighbor1 == neighbor2 || neighbor2 == id {
+		neighbor2 = RandInt()
+	}
+	return [2]int{neighbor1, neighbor2}
+}
+
+func RandInt() int {
+	// rand.Seed(time.Now().UnixNano())
+	return rand.Intn(MAX_NODES)
+}
+
+/*---------------*/
+
+// Membership struct represents participanting nodes
+type Membership struct {
+	Members map[int]Node
+}
+
+// Returns a new instance of a Membership (pointer).
+func NewMembership() *Membership {
+	return &Membership{
+		Members: make(map[int]Node),
+	}
+}
+
+// Adds a node to the membership list.
+func (m *Membership) Add(payload Node, reply *Node) error {
+	// if _, exists := m.Members[payload.ID]; exists {
+	// 	return fmt.Errorf("Node with ID %d already exists", payload.ID)
+	// }
+
+	m.Members[payload.ID] = payload
+	*reply = payload
+	return nil
+}
+
+// Updates a node in the membership list.
+func (m *Membership) Update(payload Node, reply *Node) error {
+	// if _, exists := m.Members[payload.ID]; !exists {
+	// 	return fmt.Errorf("Node with ID %d doesn't exist", payload.ID)
+	// }
+
+	m.Members[payload.ID] = payload
+	*reply = payload
+	return nil
+}
+
+// Returns a node with specific ID.
+func (m *Membership) Get(payload int, reply *Node) error {
+	if _, exists := m.Members[payload]; !exists {
+		return fmt.Errorf("Node with ID %d doesn't exist", payload)
+	}
+
+	*reply = m.Members[payload]
+	return nil
+}
+
+/*---------------*/
+
+// Request struct represents a new message request to a client
+type Request struct {
+	ID    int
+	Table Membership
+}
+
+// Requests struct represents pending message requests
+type Requests struct {
+	Pending map[int]Membership
+}
+
+// Returns a new instance of a Membership (pointer).
+func NewRequests() *Requests {
+	return &Requests{
+		Pending: make(map[int]Membership),
+	}
+}
+
+// Adds a new message request to the pending list
+func (req *Requests) Add(payload Request, reply *bool) error {
+	req.Pending[payload.ID] = payload.Table
+	*reply = true
+	return nil
+}
+
+// Listens to communication from neighboring nodes.
+func (req *Requests) Listen(ID int, reply *Membership) error {
+	if table, exists := req.Pending[ID]; exists {
+		// combined := combineTables(&table, reply)
+		*reply = table
+		delete(req.Pending, ID)
+		return nil
+	}
+	// return fmt.Errorf("no pending request with ID %d", ID)
+	return nil
+}
+
+func CombineTables(table1 *Membership, table2 *Membership) *Membership {
+	combined := NewMembership()
+
+	for id, node := range table1.Members {
+		combined.Members[id] = node
+	}
+
+	for id, node := range table2.Members {
+		if existingNode, exists := combined.Members[id]; exists {
+			if node.Hbcounter > existingNode.Hbcounter {
+				combined.Members[id] = node
+			}
+		} else {
+			combined.Members[id] = node
+		}
+	}
+
+	return combined
+}
